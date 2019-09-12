0b76e10098e817881e503f4d7d7f9c7ac0dc954a
dima <Zero30060@gmail.com>
2019-09-12 15:14:55 +0300

    uplote linear

---
+++ linear/src/lesson7/main.java
@@ -0,0 +1,18 @@
+package lesson7;
+
+public class main {
+    public static void main(String[] args) {
+        int x = 75;
+
+        int y = 0x4b;
+
+        int z = 0113;
+
+        int a = 0b1001011;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+    }
+}
---
+++ linear/src/lesson8/main.java
@@ -0,0 +1,9 @@
+package lesson8;
+
+public class main {
+    public static void main(String[] args) {
+        int z = 0113;
+
+        System.out.println(z);
+    }
+}
